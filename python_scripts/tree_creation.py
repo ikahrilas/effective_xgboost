f03637320dd07e47f2d172e818cb6890fbb42091
Ian Kahrilas <ijk60068@gmail.com>
2023-09-26 17:04:55 -0500

    XGboost stump model, comparison to dummy model

--- python_scripts/tree_creation.py
+++ python_scripts/tree_creation.py
@@ -2,6 +2,22 @@ import numpy as np
 import numpy.random as rn
 import pandas as pd
 import matplotlib.pyplot as plt
+import dtreeviz
+import urllib.request
+import zipfile
+from feature_engine import encoding, imputation
+from sklearn import base, pipeline
+from sklearn import model_selection
+
+from sklearn import tree
+from sklearn import dummy
+from sklearn import preprocessing
+import xgboost as xgb
+from xgboost_tweak import my_dot_export
+from xgboost_tweak import TweakKagTransformer
+from xgboost_tweak import get_rawX_y
+from xgboost_tweak import extract_zip
+
 
 # generate random samples using numpy
 pos_center = 12
@@ -110,3 +126,108 @@ pd.Series(ginis, index=values).loc[9.5:10.5]
 
 # values of 9.9 to 10.2 will minimize the Gini coefficient
 pd.DataFrame({'gini':ginis, 'split':values}).query('gini == gini.min()')
+
+# Making a decision tree with a single node 
+stump = tree.DecisionTreeClassifier(max_depth = 1)
+
+stump.fit(gini[['value']], gini['label'])
+
+fig, ax = plt.subplots(figsize = (8,4))
+tree.plot_tree(stump, feature_names = ['value'],
+               filled = True,
+               class_names = list(stump.classes_),
+               ax = ax)
+
+## now using XGBoost
+xg_stump = xgb.XGBClassifier(n_estiamtors = 1, max_depth = 1)
+xg_stump.fit(gini[['value']], gini.label == 'pos')
+
+xgb.plot_tree(xg_stump, num_trees = 0)
+
+my_dot_export(xg_stump, num_trees=0, filename='img/stump_xg.dot', title='A demo stump')
+
+viz = dtreeviz.model(xg_stump, X_train=gini[['value']], 
+                     y_train=gini.label=='pos',
+    target_name='positive',
+    feature_names=['value'], class_names=['negative', 'positive'],
+    tree_index=0)
+viz.view()
+
+## using kaggle data for our stump ##
+# reusable function to extract zip files
+url = 'https://github.com/mattharrison/datasets/raw/master/data/kaggle-survey-2018.zip'
+
+fname = 'kaggle-survey-2018.zip'
+member_name = 'multipleChoiceResponses.csv'
+
+extract_zip(url, fname, member_name)
+
+## Create a pipeline
+kag_pl = pipeline.Pipeline(
+    [('tweak', TweakKagTransformer()),
+     ('cat', encoding.OneHotEncoder(top_categories=5, drop_last=True, 
+           variables=['Q1', 'Q3', 'major'])),
+     ('num_impute', imputation.MeanMedianImputer(imputation_method='median',
+          variables=['education', 'years_exp']))]
+    )
+
+raw = extract_zip(url, fname, member_name)
+
+# run the code
+kag_X, kag_y = get_rawX_y(raw, 'Q6')
+
+kag_X_train, kag_X_test, kag_y_train, kag_y_test = \
+    model_selection.train_test_split(
+        kag_X, kag_y, test_size=.3, random_state=42, stratify=kag_y
+    )
+    
+X_train = kag_pl.fit_transform(kag_X_train, kag_y_train)
+X_test = kag_pl.transform(kag_X_test)
+
+stump_dt = tree.DecisionTreeClassifier(max_depth=1)
+X_train = kag_pl.fit_transform(kag_X_train)
+stump_dt.fit(X_train, kag_y_train)
+
+fig, ax = plt.subplots(figsize=(8, 4))
+features = list(c for c in X_train.columns)
+tree.plot_tree(stump_dt, feature_names=features, 
+               filled=True, 
+               class_names=list(stump_dt.classes_),
+               ax=ax)
+
+X_test = kag_pl.transform(kag_X_test)
+
+X_test
+
+stump_dt.score(X_test, kag_y_test)
+
+# dummy classifier for comparison
+dummy_model = dummy.DummyClassifier()
+dummy_model.fit(X_train, kag_y_train)
+
+dummy_model.score(X_test, kag_y_test)
+## 0.55, so quite a bit worse than our stump
+
+# use xgboost with a single tree
+kag_stump = xgb.XGBClassifier(n_estimators=1, max_depth=1)
+kag_stump.fit(X_train, kag_y_train)
+## throws error! need to handle labels
+
+label_encoder = preprocessing.LabelEncoder()
+y_train = label_encoder.fit_transform(kag_y_train)
+y_test = label_encoder.transform(kag_y_test)
+
+label_encoder.classes_
+## 0 = Data Scientist, 1 = Software Engineer 
+
+kag_stump.fit(X_train, y_train)
+kag_stump.score(X_test, y_test)
+## .55, same as dummy classifier
+
+my_dot_export(kag_stump, num_trees = 0, filename = 'img/xgb_kag_stump.dot', title = 'XGBoost stump')
+
+fig, ax = plt.subplots(figsize=(8, 4))
+tree.plot_tree(kag_stump, feature_names=features, 
+               filled=True, 
+               class_names=list(label_encoder.classes_),
+               ax=ax)
